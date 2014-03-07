16526803d2d1144fa2ad3b05b374a5f8504e2982
blivesta <yasuyukienomoto@gmail.com>
2014-03-07 23:55:49 +0900

    0.1.0

---
+++ assets/js/common.js
@@ -0,0 +1,49 @@
+$(function (){
+
+// cooker
+// ==================================================
+
+/*
+  $('#input').inputcounters({
+      warningCounter:2,
+      errorCounter:4
+  });
+*/
+
+//	$('a[href^=#]').scrollmethods();
+
+/*
+	$('.dropdown').dropdowns();
+
+	$('.tooltips').tooltips();
+*/
+	
+	$('.switchers').switchers();
+	
+/*
+	$('.slidebar_right').slidebars({
+      position: 'right',
+	});
+	
+	$('.slidebar_bottom').slidebars({
+    position: 'bottom',
+	});
+*/
+	
+// bootstrap.affix
+// ==================================================
+
+  $('body').scrollspy({ target: '.ck-docs-sidenav' })
+
+  $('.ck-docs-sidebar').affix({
+    offset: {
+      top: function () {
+          return this.bottom = $('.ck-docs-nav').outerHeight() + $('.ck-docs-header').outerHeight();
+      }, 
+    }
+  });
+
+	
+	
+});
+
---
+++ assets/js/cooker.js
@@ -0,0 +1,78 @@
+/*!
+ * cooker v0.1.0
+ * Copyright 2014 TEQ inc.
+ * Licensed under ,, (,,)
+ */
+(function($) {
+  var namespace = "switchers";
+  var methods = {
+    init: function(options) {
+      options = $.extend({
+        switcherClass: "switcher",
+        toggleClass: "switcher-toggle",
+        allToggleClass: "switcher-toggle-all",
+        activeClass: "switcher-active"
+      }, options);
+      return this.each(function() {
+        var _this = this;
+        var $this = $(this);
+        var data = $this.data(namespace);
+        if (!data) {
+          options = $.extend({}, options);
+          $this.data(namespace, {
+            options: options
+          });
+        }
+        var $aToggle = $this.find("." + options.allToggleClass);
+        var $iToggle = $this.find("." + options.toggleClass);
+        var switcher = options.switcherClass;
+        var active = options.activeClass;
+        $aToggle.off("click").on("click", function() {
+          var $this = $(this);
+          var $a = $this.parents("." + namespace).find("." + switcher);
+          if ($a.hasClass(active)) {
+            $a.removeClass(active);
+          } else {
+            $a.addClass(active);
+          }
+        });
+        $iToggle.off("click").on("click", function() {
+          var $this = $(this);
+          var mode = $this.parents("." + namespace).data("mode");
+          var $i = $this.parents("." + switcher);
+          var $o = $i.siblings();
+          if (mode === "accordion") {
+            if ($i.hasClass(active)) {
+              $o, $i.removeClass(active);
+            } else {
+              $o.removeClass(active);
+              $i.addClass(active);
+            }
+          } else {
+            if ($i.hasClass(active)) {
+              $i.removeClass(active);
+            } else {
+              $i.addClass(active);
+            }
+          }
+        });
+      });
+    },
+    destroy: function() {
+      return this.each(function() {
+        var $this = $(this);
+        $(window).unbind("." + namespace);
+        $this.removeData(namespace);
+      });
+    }
+  };
+  $.fn.switchers = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === "object" || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error("Method " + method + " does not exist on jQuery." + namespace);
+    }
+  };
+})(jQuery);
