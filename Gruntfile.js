458f8877e1152616325ff54d614323ad44333a0d
blivesta <yasuyukienomoto@gmail.com>
2014-03-07 23:24:39 +0900

    grunt & jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,14 +176,14 @@
     htmlmin: {
       dist: {
         options: {
-            collapseBooleanAttributes: true,
-            collapseWhitespace: true,
-            removeAttributeQuotes: true,
-            removeCommentsFromCDATA: true,
-            removeEmptyAttributes: true,
-            removeOptionalTags: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true
         },
         files: [{
             expand: true,
