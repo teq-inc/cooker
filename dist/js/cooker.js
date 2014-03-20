b312471c7a28001cd9a5f0fd356bb38b0e8987cd
blivesta <yasuyukienomoto@gmail.com>
2014-03-21 04:29:10 +0900

    update drawer

--- dist/js/cooker.js
+++ dist/js/cooker.js
@@ -157,6 +157,9 @@
         $window.resize(function() {
           methods.resize.call(_this, "resize");
         });
+        $nav.find("a").off("click." + namespace).on("click." + namespace, function() {
+          methods.close.call(_this);
+        });
         if (touches) {
           $toggle.bind("touchstart." + namespace, function(e) {
             methods.toggle.apply(_this);
